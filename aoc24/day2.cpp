75a435b142094a4362786048e2c8269556ec830a
matej <matejzavrsnik@gmail.com>
2024-12-02 07:48:18 +0000

    day 2 part 1

--- aoc24/day2.cpp
+++ aoc24/day2.cpp
@@ -4,11 +4,22 @@
 
 using namespace aoc24;
 
+TEST(aoc24, d2_p1_cases)
+{
+   auto result = d2::p1({
+      "1 2 3",
+      "3 2 1",
+      "1 4 5",
+      "1 1 3"
+   });
+   ASSERT_EQ(3, result);
+}
+
 TEST(aoc24, d2_p1)
 {
    vec<string> input = read_file_lines("./res/aoc24/day2.input.txt");
    auto result = d2::p1(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(383, result);
 }
 
 TEST(aoc24, d2_p2)
--- aoc24/day2.h
+++ aoc24/day2.h
@@ -4,17 +4,35 @@
 
 namespace aoc24::d2
 {
-
 inline ll
-p1 (vec<string> input)
-{
-   return 1;
+p1 (vec<string> input) {
+   ll count_safe = 0;
+   for (auto report : input) {
+      ll direction = 0;
+      auto safe_level = sr::all_of(
+         split(report, " ")
+         | sv::transform(strv_to_ll)
+         | std::views::adjacent<2>
+         | sv::transform([&direction] (auto tup) {
+            auto [prev, next] = tup;
+            if (prev == next) return false; /// unsafe
+            if (direction == 0) direction = (next - prev) / std::abs(next - prev);
+            bool safe = direction == 1
+                        ? (next > prev && next <= (prev + 3))
+                        : (next < prev && next >= (prev - 3));
+            return safe;
+         }),
+         [] (auto safe) { return safe == true; });
+
+      if (safe_level) ++count_safe;
+   }
+   return count_safe;
 }
 
 inline ll
 p2 (vec<string> input)
 {
-   return 1;
+   return 0;
 }
 
 }
