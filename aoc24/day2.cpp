4f9a25d1ed854f078e250d8e7894a0eae64b0882
matej <matejzavrsnik@gmail.com>
2024-12-02 08:21:22 +0000

    day 2 part 2

--- aoc24/day2.cpp
+++ aoc24/day2.cpp
@@ -4,17 +4,6 @@
 
 using namespace aoc24;
 
-TEST(aoc24, d2_p1_cases)
-{
-   auto result = d2::p1({
-      "1 2 3",
-      "3 2 1",
-      "1 4 5",
-      "1 1 3"
-   });
-   ASSERT_EQ(3, result);
-}
-
 TEST(aoc24, d2_p1)
 {
    vec<string> input = read_file_lines("./res/aoc24/day2.input.txt");
@@ -26,5 +15,6 @@ TEST(aoc24, d2_p2)
 {
    vec<string> input = read_file_lines("./res/aoc24/day2.input.txt");
    auto result = d2::p2(input);
-   ASSERT_EQ(1, result);
+   ASSERT_GT(506, result); // 506 too high
+   ASSERT_EQ(436, result);
 }
--- aoc24/day2.h
+++ aoc24/day2.h
@@ -29,10 +29,47 @@ p1 (vec<string> input) {
    return count_safe;
 }
 
+inline bool is_safe (vec<ll> report)
+{
+   ll direction = 0;
+   bool safe = sr::all_of(
+      report
+      | std::views::adjacent<2>
+      | sv::transform([&direction] (auto tup) {
+         auto [prev, next] = tup;
+         if (prev == next) return false; // unsafe
+         if (direction == 0) direction = (next - prev) / std::abs(next - prev);
+         bool safe = direction == 1
+                     ? (next > prev && next <= (prev + 3))
+                     : (next < prev && next >= (prev - 3));
+         return safe;
+      }),
+     [] (auto safe) { return safe == true; });
+   return safe;
+};
+
+inline bool dampener_safe(vec<ll> report) {
+   if (is_safe(report)) return true;
+   for (int i=0; i<report.size(); ++i) {
+      auto dampened_report = report;
+      dampened_report.erase(dampened_report.begin()+i);
+      if (is_safe(dampened_report)) return true;
+   }
+   return false;
+}
+
 inline ll
 p2 (vec<string> input)
 {
-   return 0;
+   ll count_safe = 0;
+   for (auto report_strings : input) {
+      vec<ll> report = split(report_strings, " ")
+         | sv::transform(strv_to_ll)
+         | sr::to<vec<ll>>();
+
+      if (dampener_safe(report)) ++count_safe;
+   }
+   return count_safe;
 }
 
 }
