3f2b2c7ab7c62d2ea4e27684a8f6b4cd2638ce51
matej <matejzavrsnik@gmail.com>
2024-12-05 09:05:31 +0000

    day 4

--- aoc24/day4.cpp
+++ aoc24/day4.cpp
@@ -8,12 +8,45 @@ TEST(aoc24, d4_p1)
 {
    vec<string> input = read_file_lines("./res/aoc24/day4.input.txt");
    auto result = d4::p1(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(2599, result);
+}
+
+TEST(aoc24, d4_p2_cases) {
+   //example
+   auto result = d4::p2({
+      ".M.S......",
+      "..A..MSMS.",
+      ".M.S.MAA..",
+      "..A.ASMSM.",
+      ".M.S.M....",
+      "..........",
+      "S.S.S.S.S.",
+      ".A.A.A.A..",
+      "M.M.M.M.M.",
+      ".........."
+   });
+   ASSERT_EQ(9, result);
+
+   // corners
+   result = d4::p2({
+      "M.S....M.M",
+      ".A......A.",
+      "M.S....S.S",
+      "..........",
+      "..........",
+      "..........",
+      "..........",
+      "S.M....S.S",
+      ".A......A.",
+      "S.M....M.M"
+   });
+   ASSERT_EQ(4, result);
 }
 
 TEST(aoc24, d4_p2)
 {
    vec<string> input = read_file_lines("./res/aoc24/day4.input.txt");
    auto result = d4::p2(input);
-   ASSERT_EQ(1, result);
+   ASSERT_LT(1886, result); // too low
+   ASSERT_EQ(1948, result);
 }
--- aoc24/day4.h
+++ aoc24/day4.h
@@ -5,16 +5,99 @@
 namespace aoc24::d4
 {
 
+inline bool
+xmas(const gridt<char> g, cell c, direction d) {
+   string s;
+   for (auto step=0; step<4; ++step) {
+      if (!grid::is_in(g, c)) return false;
+      s += grid::access(g, c);
+      c = grid::move_screen(c, d);
+   }
+   return s == "XMAS";
+}
+
 inline ll
 p1 (vec<string> input)
 {
-   return 1;
+   const gridt<char> g = grid::convert_to_grid<char>(input);
+   int count=0;
+   for (int w = 0; w < grid::width(g); ++w)
+      for (int h = 0; h < grid::width(g); ++h) {
+         cell c{w, h};
+         if (xmas(g, c, direction::e)) ++count;
+         if (xmas(g, c, direction::se)) ++count;
+         if (xmas(g, c, direction::s)) ++count;
+         if (xmas(g, c, direction::sw)) ++count;
+         if (xmas(g, c, direction::w)) ++count;
+         if (xmas(g, c, direction::nw)) ++count;
+         if (xmas(g, c, direction::n)) ++count;
+         if (xmas(g, c, direction::ne)) ++count;
+      }
+   return count;
+}
+
+template <typename T>
+bool
+check_for_drawing (
+   const mzlib::grid::type<T>& grid,
+   const mzlib::grid::type<T>& drawing,
+   const mzlib::grid::cell& c,
+   T neutral_value = '.'
+)
+{
+   mzlib::grid::cell drawing_pixel{0, 0};
+   const mzlib::grid::cell drawing_size = mzlib::grid::size(drawing);
+   do
+   {
+      auto canvas_pixel = drawing_pixel + c;
+      const auto pixel_value = mzlib::grid::access(drawing, drawing_pixel);
+      if (pixel_value == neutral_value)
+         continue;
+      const auto value_on_grid = mzlib::grid::access(grid, canvas_pixel);
+      if (value_on_grid != pixel_value)
+         return false;
+   }
+   while (mzlib::grid::move_to_next_cell(drawing_pixel, {0, 0}, drawing_size));
+
+   return true;
 }
 
 inline ll
 p2 (vec<string> input)
 {
-   return 1;
+   const gridt<char> g = grid::convert_to_grid<char>(input);
+   int count=0;
+
+   vec<gridt<char>> xmases = {
+      {
+         {'M', '.', 'S'},
+         {'.', 'A', '.'},
+         {'M', '.', 'S'}
+      },
+      {
+         {'M', '.', 'M'},
+         {'.', 'A', '.'},
+         {'S', '.', 'S'}
+      },
+      {
+         {'S', '.', 'M'},
+         {'.', 'A', '.'},
+         {'S', '.', 'M'}
+      },
+      {
+         {'S', '.', 'S'},
+         {'.', 'A', '.'},
+         {'M', '.', 'M'}
+      },
+   };
+
+   for (auto xmas : xmases)
+      for (int w = 0; w < grid::width(g)-2; ++w)
+         for (int h = 0; h < grid::width(g)-2; ++h) {
+            cell c{w, h};
+            if (check_for_drawing(g, xmas, c)) ++count;
+         }
+   return count;
 }
 
 }
