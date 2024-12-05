e1760a2de716b3292c48234f5859ebd2c2021f91
matej <matejzavrsnik@gmail.com>
2024-12-05 14:24:54 +0000

    day 5

--- aoc24/day5.cpp
+++ aoc24/day5.cpp
@@ -8,12 +8,12 @@ TEST(aoc24, d5_p1)
 {
    vec<string> input = read_file_lines("./res/aoc24/day5.input.txt");
    auto result = d5::p1(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(4790, result);
 }
 
 TEST(aoc24, d5_p2)
 {
    vec<string> input = read_file_lines("./res/aoc24/day5.input.txt");
    auto result = d5::p2(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(6319, result);
 }
--- aoc24/day5.h
+++ aoc24/day5.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <syncstream>
+
 #include "everything_bagel.h"
 
 namespace aoc24::d5
@@ -8,13 +10,84 @@ namespace aoc24::d5
 inline ll
 p1 (vec<string> input)
 {
-   return 1;
+   auto input_rules = std::views::filter(input, [](auto s){ return s.contains('|'); });
+   auto input_updates= std::views::filter(input, [](auto s){ return s.contains(','); });
+
+   map<string, set<string>> must_come_after;
+   for (auto input_rule : input_rules) {
+      vec<string> parts = absl::StrSplit(input_rule, absl::ByAnyChar("|"), absl::SkipEmpty());
+      must_come_after[parts[0]].insert(parts[1]);
+   }
+
+   vec<vec<string>> updates;
+   for (auto input_update : input_updates) {
+      vec<string> update = absl::StrSplit(input_update, absl::ByAnyChar(","), absl::SkipEmpty());
+      updates.push_back(update);
+   }
+
+   auto is_correctly_ordered = [&must_come_after](vec<string> update) {
+      set<string> came_before;
+      for ( auto num: update) {
+         auto overlap = sets_intersection(came_before, must_come_after[num]);
+         if (!overlap.empty()) return false;
+         came_before.insert(num);
+      }
+      return true;
+   };
+
+   ll sum=0;
+   for (auto update : updates)
+      if (is_correctly_ordered(update))
+         sum += stoll(update[update.size()/2]);
+
+   return sum;
 }
 
 inline ll
 p2 (vec<string> input)
 {
-   return 1;
+   auto input_rules = std::views::filter(input, [](auto s){ return s.contains('|'); });
+   auto input_updates= std::views::filter(input, [](auto s){ return s.contains(','); });
+
+   map<string, set<string>> must_come_after;
+   for (auto input_rule : input_rules) {
+      vec<string> parts = absl::StrSplit(input_rule, absl::ByAnyChar("|"), absl::SkipEmpty());
+      must_come_after[parts[0]].insert(parts[1]);
+   }
+
+   vec<vec<string>> updates;
+   for (auto input_update : input_updates) {
+      vec<string> update = absl::StrSplit(input_update, absl::ByAnyChar(","), absl::SkipEmpty());
+      updates.push_back(update);
+   }
+
+   auto is_correctly_ordered = [&must_come_after](vec<string> update) {
+      set<string> came_before;
+      for ( auto num: update) {
+         auto overlap = sets_intersection(came_before, must_come_after[num]);
+         if (!overlap.empty()) return false;
+         came_before.insert(num);
+      }
+      return true;
+   };
+
+   auto order_it = [&must_come_after](vec<string> update) {
+      sr::sort(update, [&must_come_after](string prev, string next) {
+         if (must_come_after[prev].contains(next))
+            return true; // "true if the first argument is less", them's the rules
+         return false;
+      });
+      return update;
+   };
+
+   ll sum=0;
+   for (auto update : updates)
+      if (!is_correctly_ordered(update)) {
+         update = order_it(update);
+         sum += stoll(update[update.size()/2]);
+      }
+
+   return sum;
 }
 
 }
