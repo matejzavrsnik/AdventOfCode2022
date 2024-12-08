a92027f9293954b598ce0c97d27c6bc857f8d742
matej <matejzavrsnik@gmail.com>
2024-12-08 07:24:51 +0000

    day 8

--- aoc24/day8.cpp
+++ aoc24/day8.cpp
@@ -4,16 +4,55 @@
 
 using namespace aoc24;
 
+TEST(aoc24, d8_p1_example) {
+   vec<string> input = {
+      "............",
+      "........0...",
+      ".....0......",
+      ".......0....",
+      "....0.......",
+      "......A.....",
+      "............",
+      "............",
+      "........A...",
+      ".........A..",
+      "............",
+      "............"
+   };
+   auto result = d8::p1(input);
+   ASSERT_EQ(14, result);
+}
+
 TEST(aoc24, d8_p1)
 {
    vec<string> input = read_file_lines("./res/aoc24/day8.input.txt");
    auto result = d8::p1(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(336, result);
+}
+
+TEST(aoc24, d8_p2_example)
+{
+   vec<string> input = {
+      "............",
+      "........0...",
+      ".....0......",
+      ".......0....",
+      "....0.......",
+      "......A.....",
+      "............",
+      "............",
+      "........A...",
+      ".........A..",
+      "............",
+      "............"
+   };
+   auto result = d8::p2(input);
+   ASSERT_EQ(34, result);
 }
 
 TEST(aoc24, d8_p2)
 {
    vec<string> input = read_file_lines("./res/aoc24/day8.input.txt");
    auto result = d8::p2(input);
-   ASSERT_EQ(1, result);
+   ASSERT_EQ(1131, result); // 1000 too low
 }
--- aoc24/day8.h
+++ aoc24/day8.h
@@ -8,13 +8,61 @@ namespace aoc24::d8
 inline ll
 p1 (vec<string> input)
 {
-   return 1;
+   auto g = grid::convert_to_grid<char>(input);
+
+   // get all cells with any frequency
+   map<char, set<cell>> freq_loc;
+   auto all_cells = grid::get_all_cells(g);
+   for (auto cell : all_cells)
+      if (grid::access(g, cell) != '.')
+         freq_loc[grid::access(g, cell)].insert(cell);
+
+   // for each pair, calculate # using distance * 2
+   set<cell> antinodes;
+   for (auto [freq, locations] : freq_loc)
+      for (auto loc1 : locations)
+         for (auto loc2 : locations)
+            if (loc1 != loc2) {
+               // A[5,2] -> B[4,4] -> [3,6] = [4-5+4, 4-2+4] = (B-A)+B
+               auto antinode_loc = loc2+(loc2-loc1);
+               if (grid::is_in(g, antinode_loc))
+                     antinodes.insert(antinode_loc);
+            }
+
+   // set of #, size, is result
+   return antinodes.size();
 }
 
 inline ll
 p2 (vec<string> input)
 {
-   return 1;
+   auto g = grid::convert_to_grid<char>(input);
+
+   // get all cells with any frequency
+   map<char, set<cell>> freq_loc;
+   auto all_cells = grid::get_all_cells(g);
+   for (auto cell : all_cells)
+      if (grid::access(g, cell) != '.')
+         freq_loc[grid::access(g, cell)].insert(cell);
+
+   // for each pair, calculate # using distance * 2
+   set<cell> antinodes;
+   for (auto [freq, locations] : freq_loc)
+      for (auto loc1 : locations)
+         for (auto loc2 : locations)
+            if (loc1 != loc2) {
+               // A[5,2] -> B[4,4] -> [3,6] = [4-5+4, 4-2+4] = (B-A)+B
+               auto increment = loc2-loc1;
+               cell antinode_loc = loc2;
+               while (grid::is_in(g, antinode_loc)) {
+                  antinodes.insert(antinode_loc);
+                  grid::access(g, antinode_loc) = '#';
+                  antinode_loc += increment;
+               }
+            }
+
+   // set of #, size, is result
+   return antinodes.size();
 }
 
 }
